756224390f83be254b9c72e477063171a9dd15e2
kai.dai <kai.dai@qunar.com>
2019-11-10 19:02:16 +0800

    添加演示文件

---
+++ src/com/dk/pyramid/demo/FormatFieldWithPyramidDemo.java
@@ -0,0 +1,18 @@
+package com.dk.pyramid.demo;
+
+import javax.annotation.Resource;
+
+/**
+ * @author : kai.dai
+ * @date : 2019-11-10 17:52
+ */
+public class FormatFieldWithPyramidDemo {
+    /**
+     * demo int
+     */
+    private int b;
+    private boolean a;
+    @Resource
+    int i;
+    private String str;
+}
